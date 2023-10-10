871a4a41d11dc643b425f6305a5d6d6a9c26163a
Noémie <noemie@mediawind.be>
2023-10-10 09:33:19 +0200

    fix: route variable update

--- utils/getRoute.ts
+++ utils/getRoute.ts
@@ -1,10 +1,10 @@
-import { FlowVariables } from "../variables";
+import { Variables } from "../../variables";
 
 import { Route } from "../interfaces";
 
 export default async function getRoute() {
 
-	const route = await import(`../../routes/${FlowVariables.W_ROUTE}`);
+	const route = await import(`../../routes/${Variables.W_ROUTE}`);
 
 	return route.default as Route;
 }
--- variables.ts
+++
@@ -1,5 +0,0 @@
-import { DefaultVariables } from "../core/variables";
-
-export class FlowVariables extends DefaultVariables {
-	static W_ROUTE = (process.env.NODE_ENV === "production" ? "{w_route}" : "route_31.json") as string;
-}
