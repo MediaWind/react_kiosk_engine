eb0a45650eae6aed0d4f0d984d132b70e114d8e8
Noémie <noemie@mediawind.be>
2023-10-20 13:50:58 +0200

    fix: deleted useless getRoute file

--- utils/getRoute.ts
+++
@@ -1,8 +0,0 @@
-import { Variables } from "../../variables";
-
-import { Route } from "../interfaces";
-
-export default async function getRoute() {
-	const route = await import(`../../routes/${Variables.W_ROUTE}`);
-	return route.default as Route;
-}
