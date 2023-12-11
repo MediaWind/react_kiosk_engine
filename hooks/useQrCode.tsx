7baa7f8f57835832ba2b2dd48fd5081c0b90edef
Noémie <noemie@mediawind.be>
2023-12-11 14:01:01 +0100

    feat: qr code ticket print init

--- hooks/useQrCode.tsx
+++ hooks/useQrCode.tsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
 
 import { Variables } from "../../variables";
+
 import { APPOINTMENT_ACTION_TYPE, IAppointmentAction } from "../interfaces";
+import Printer from "../../core/client/printer";
 
 export default function useQrCode(dispatchAppointment: React.Dispatch<IAppointmentAction>): [CallableFunction] {
 	const [qrCodeText, setQrCodeText] = useState<string>("");
@@ -45,6 +47,28 @@ export default function useQrCode(dispatchAppointment: React.Dispatch<IAppointme
 					type: APPOINTMENT_ACTION_TYPE.UPDATECHECKEDIN,
 					payload: true,
 				});
+
+				try {
+					const result = await Printer.print(data.pdf);
+
+					if (result) {
+						// setTimeout(() => {
+						// 	setIsPrinting(false);
+						// }, 5000);
+					}
+				} catch (err) {
+					// setIsPrinting(false);
+
+					console.log(err);
+					// dispatchError({
+					// 	type: ERROR_ACTION_TYPE.SETERROR,
+					// 	payload: {
+					// 		hasError: true,
+					// 		errorCode: ERROR_CODE.A500,
+					// 		message: "Could not print ticket",
+					// 	},
+					// });
+				}
 			}
 		} catch (err) {
 			console.log(err);
