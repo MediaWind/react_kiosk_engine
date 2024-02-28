fea7d58ea9e6692d7f4b1b02df09612fb5bd17ea
Noémie Goosen <noemie@mediawind.be>
2024-02-28 09:45:23 +0100

    feat: added preview print simulation

--- hooks/useTicket.tsx
+++ hooks/useTicket.tsx
@@ -1,6 +1,8 @@
+import { Variables } from "../../variables";
 import { ERROR_ACTION_TYPE, ERROR_CODE, IErrorAction, IFlow, IPrintAction, ITicketDataState, PRINT_ACTION_TYPE } from "../interfaces";
 
 import getTicketingURL from "../utils/getTicketingURL";
+import { testPDF } from "../utils/testPDF";
 
 export default function useTicket(dispatchPrintState: React.Dispatch<IPrintAction>, dispatchError: React.Dispatch<IErrorAction>): [CallableFunction] {
 	async function createTicket(ticketState: ITicketDataState, flow: IFlow) {
@@ -16,27 +18,34 @@ export default function useTicket(dispatchPrintState: React.Dispatch<IPrintActio
 					payload: data.pdf,
 				});
 			} else {
-				console.log("Error: unable to create ticket. Data:", data);
+				if (!Variables.PREVIEW) {
+					console.log("Error: unable to create ticket. Data:", data);
 
-				//TODO: add more error reason diversity
-				if (data.status_reason == "service_closed") {
-					dispatchError({
-						type: ERROR_ACTION_TYPE.SETERROR,
-						payload: {
-							hasError: true,
-							errorCode: ERROR_CODE.C500,
-							message: "Service closed",
-							errorServiceId: ticketState.service ? `${ticketState.service.serviceID}` : undefined,
-						},
-					});
+					//TODO: add more error reason diversity
+					if (data.status_reason == "service_closed") {
+						dispatchError({
+							type: ERROR_ACTION_TYPE.SETERROR,
+							payload: {
+								hasError: true,
+								errorCode: ERROR_CODE.C500,
+								message: "Service closed",
+								errorServiceId: ticketState.service ? `${ticketState.service.serviceID}` : undefined,
+							},
+						});
+					} else {
+						dispatchError({
+							type: ERROR_ACTION_TYPE.SETERROR,
+							payload: {
+								hasError: true,
+								errorCode: ERROR_CODE.B500,
+								message: "Unable to create ticket",
+							},
+						});
+					}
 				} else {
-					dispatchError({
-						type: ERROR_ACTION_TYPE.SETERROR,
-						payload: {
-							hasError: true,
-							errorCode: ERROR_CODE.B500,
-							message: "Unable to create ticket",
-						},
+					dispatchPrintState({
+						type: PRINT_ACTION_TYPE.UPDATETICKETPDF,
+						payload: testPDF,
 					});
 				}
 			}
