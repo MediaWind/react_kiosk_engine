0ab2a9bdb62f5ab05938d5d086acef0a6c554db4
Noémie Goosen <noemie@mediawind.be>
2024-03-05 13:45:40 +0100

    refactor: removed error dispatch

--- utils/fetchRetry.ts
+++ utils/fetchRetry.ts
@@ -1,32 +1,21 @@
 import dayjs from "dayjs";
-import { ERROR_ACTION_TYPE, IErrorAction } from "../interfaces";
-import { ERROR_CODE } from "../lib/errorCodes";
 
-export default async function fetchRetry(url: URL, errorDispatch?: React.Dispatch<IErrorAction>, retries = 0) {
+export default async function fetchRetry(url: URL | string, retries = 0): Promise<Response | undefined> {
+	let response;
+
 	try {
-		const response = await fetch(url);
+		response = await fetch(url);
 
-		if (response.status !== 200 && retries < 3) {
+		if (!response.ok && retries < 3) {
 			setTimeout(() => {
-				fetchRetry(url, errorDispatch, retries++);
+				fetchRetry(url, retries + 1);
 			}, 100);
 		} else if (retries >= 3) {
-			if (errorDispatch) {
-				errorDispatch({
-					type: ERROR_ACTION_TYPE.SETERROR,
-					payload: {
-						hasError: true,
-						errorCode: ERROR_CODE.A429,
-						message: "Too many fetch retries",
-					},
-				});
-			} else {
-				throw Error("Too many fetch retries");
-			}
+			throw new Error("Too many fetch retries");
 		} else {
 			return response;
 		}
 	} catch (err) {
-		console.log(dayjs().unix() + " - fetchRetry:30 - error: " + err);
+		console.log(dayjs().unix() + " - fetchRetry:19 - error: " + err);
 	}
 }
