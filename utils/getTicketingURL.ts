85081af8f9f39556415288de24d3f0ee81de4d5f
Noémie Goosen <noemie@mediawind.be>
2024-04-12 11:18:26 +0200

    feat: added fileds in ticketing url creation

--- utils/getTicketingURL.ts
+++ utils/getTicketingURL.ts
@@ -21,10 +21,16 @@ export default function getTicketingURL(ticketState: ITicketDataState, flow: IFl
 	let paramsFirstName = "";
 	let paramsLastName = "";
 	let paramsNationalNumber = "";
+	let paramsBirthDate = "";
+	let paramsAddress = "";
+	let paramsZipAndCity = "";
 
 	if (ticketState.textInputDatas) {
 		const firstName = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.firstname);
 		const lastName = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.lastname);
+		const birthDate = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.birthDate);
+		const address = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.address);
+		const zipAndCity = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.zipAndCity);
 		const nationalNumber = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.nationalNumber);
 		const email = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.email);
 		const phone = ticketState.textInputDatas.find((input) => input.id === flow.ticketParameters?.phone);
@@ -35,6 +41,9 @@ export default function getTicketingURL(ticketState: ITicketDataState, flow: IFl
 		paramsFirstName = firstName?.value ?? "";
 		paramsLastName = lastName?.value ?? "";
 		paramsNationalNumber = nationalNumber?.value ?? "";
+		paramsBirthDate = birthDate?.value ?? "";
+		paramsAddress = address?.value ?? "";
+		paramsZipAndCity = zipAndCity?.value ?? "";
 
 		params += `
 		&email=${encodeURIComponent(email?.value ?? "")}
@@ -49,12 +58,18 @@ export default function getTicketingURL(ticketState: ITicketDataState, flow: IFl
 		paramsFirstName = ticketState.eIdDatas.firstName;
 		paramsLastName = ticketState.eIdDatas.lastName;
 		paramsNationalNumber = ticketState.eIdDatas.nationalNumber;
+		paramsBirthDate = ticketState.eIdDatas.dateOfBirth;
+		paramsAddress = ticketState.eIdDatas.addressStreetAndNumber;
+		paramsZipAndCity = `${ticketState.eIdDatas.addressZip} ${ticketState.eIdDatas.addressMunicipality}`;
 	}
 
 	params += `
 	&firstname=${encodeURIComponent(paramsFirstName)}
 	&lastname=${encodeURIComponent(paramsLastName)}
 	&registre_national=${encodeURIComponent(paramsNationalNumber)}
+	&birth_date=${encodeURIComponent(paramsBirthDate)}
+	&adress=${encodeURIComponent(paramsAddress)}
+	&postal_code_city=${encodeURIComponent(paramsZipAndCity)}
 	`;
 
 	return new URL(baseURL + params);
