ea852633543f8a1a6844d17a99b89ff9359f6ba1
MaximeMediawind <maxime@mediawind.be>
2024-10-04 09:11:23 +0200

    refactor: Update getAppointments function to include service filter in params

--- hooks/useAppointment.tsx
+++ hooks/useAppointment.tsx
@@ -187,7 +187,7 @@ export default function useAppointment(dispatchAppointment: React.Dispatch<IAppo
 		}
 	}
 
-	async function getAppointments(birthDate: string | null = null, nationalNumber: string | null = null, minBeforeAppointment = 60, minAfterAppointment = 120) : Promise<object | undefined> {
+	async function getAppointments(birthDate: string | null = null, nationalNumber: string | null = null, minBeforeAppointment = 60, minAfterAppointment = 120, services: Array<number> | null = null) : Promise<object | undefined> {
 		Console.info("Getting appointments...");
 
 		let appointmentsURL = `
@@ -212,15 +212,8 @@ export default function useAppointment(dispatchAppointment: React.Dispatch<IAppo
 		// ADD FILTERS
 		if(birthDate) 		appointmentsURL += `&birth_date=${birthDate}`;
 		if(nationalNumber) 	appointmentsURL += `&registre_national=${nationalNumber}`;
-
-
-		// ADD SERVICE FILTER
-		const services = [];
-		if(Variables.W_ID_SERVICE_FILTER_LABO) 		services.push(Variables.W_ID_SERVICE_FILTER_LABO);
-		if(Variables.W_ID_SERVICE_FILTER_PEDIA) 	services.push(Variables.W_ID_SERVICE_FILTER_PEDIA);
-		if(services.length > 0) {
-			appointmentsURL += `&id_service=${services.join(",")}`;
-		}
+		if(services && services.length > 0) appointmentsURL += `&id_service=${services.join(",")}`;
+		
 
 		// add time filter
 		const timeStart = new Date();
