3a2ca0b2f12b7105703c3a092df288686e1a3ac9
MaximeMediawind <maxime@mediawind.be>
2026-02-26 09:50:43 +0100

    fix: ensure W_ID_FLOW existence check before generating KioskFlow URL

--- utils/getKioskFlowURL.ts
+++ utils/getKioskFlowURL.ts
@@ -1,5 +1,7 @@
 import { Variables } from "../../variables";
 
 export default function getKioskFlowURL(): URL {
+	if (!("W_ID_FLOW" in Variables) || !Variables.W_ID_FLOW) return new URL("");
+
 	return new URL(`${Variables.DOMAINE_HTTP}/modules/Modules/KioskFlow/services/flows/findOne.php?id_project=${Variables.W_ID_FLOW}&serial=${Variables.SERIAL}&key_protect=${Variables.KEY_PLAYER}&id_easyqueue=${Variables.W_ID_PROJECT}`);
 }
